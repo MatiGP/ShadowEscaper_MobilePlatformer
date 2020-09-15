4384bc166fd4bfb8ecab9df7b7985721dda7a7d4
MatiGP <MatiGP@users.noreply.github.com>
2020-09-15 23:32:32 +0200

    Created level selection, removed some additional assets from SampleLevel

---
+++ Assets/LevelSelection.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelSelection : MonoBehaviour
+{
+    [SerializeField] float panelSwipeTime;
+    [SerializeField] RectTransform levelPanel;
+    [SerializeField] GameObject nextButton;
+    [SerializeField] GameObject prevButton;
+
+    int currentIndex;
+
+    private void Start()
+    {
+        Debug.Log(levelPanel.transform.childCount - 2);
+    }
+
+    public void NextWorldPage()
+    {
+        currentIndex++;
+
+        if(currentIndex == levelPanel.transform.childCount - 1)
+        {
+            nextButton.SetActive(false);
+        }
+        prevButton.SetActive(true);
+
+        MoveLevelPanel(backwards: false);
+
+        
+    }
+
+    public void PreviousWorldPage()
+    {
+        currentIndex--;
+
+        if (currentIndex == 0)
+        {
+            prevButton.SetActive(false);
+        }
+        nextButton.SetActive(true);
+
+        MoveLevelPanel(backwards: true);
+
+        
+    }
+
+    void MoveLevelPanel(bool backwards)
+    {
+        if (backwards)
+        {
+            levelPanel.anchoredPosition = new Vector2(levelPanel.anchoredPosition.x + 1900, levelPanel.anchoredPosition.y);
+        }
+        else
+        {
+            levelPanel.anchoredPosition = new Vector2(levelPanel.anchoredPosition.x - 1900, levelPanel.anchoredPosition.y);
+        }       
+    }
+
+    
+
+    private void OnEnable()
+    {
+        currentIndex = 0;
+        nextButton.SetActive(true);
+        prevButton.SetActive(false);
+        levelPanel.anchoredPosition = new Vector2(0f, 0f);
+
+    }
+}
