4c411cbcfb354280e2a63d4522f1d334ae9f231e
Mateusz Świeca <mateusz.swieca@o2.pl>
2022-06-05 22:07:09 +0200

    added new platform interface

---
+++ Assets/Scripts/Level/Platforms/IPlatform.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Code
+{
+    public interface IPlatform
+    {
+        void ActivatePlatform();
+    }
+}
